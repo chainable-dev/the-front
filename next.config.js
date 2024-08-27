6e28e254191b3e865181d8129456c65846e49dae
SourC <souriya.khaosanga@gmail.com>
2024-08-26 23:29:14 -0700

    fix build

--- next.config.js
+++ next.config.js
@@ -5,7 +5,11 @@ const nextConfig = {
   images: {
     domains: ['lh3.googleusercontent.com'],
   },
-  webpack: (config) => {
+  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@': path.resolve(__dirname),
+    };
     config.resolve.fallback = { fs: false, net: false, tls: false };
     return config;
   },
